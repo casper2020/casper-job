ae379d07b7efc4feb0cf09b22ed5c20d7e10c8d1
Américo Gomes <americo.gomes@cldware.com>
2021-11-18 17:58:57 +0000

    Adding base classes - draft.

--- src/casper/job/base.h
+++ src/casper/job/base.h
@@ -3,20 +3,20 @@
 *
 * Copyright (c) 2011-2021 Cloudware S.A. All rights reserved.
 *
-* This file is part of casper-fs-worker.
+* This file is part of casper-job.
 *
-* casper-fs-worker is free software: you can redistribute it and/or modify
+* casper-job is free software: you can redistribute it and/or modify
 * it under the terms of the GNU Affero General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
 * (at your option) any later version.
 *
-* casper-fs-worker  is distributed in the hope that it will be useful,
+* casper-job is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 * GNU General Public License for more details.
 *
 * You should have received a copy of the GNU Affero General Public License
-* along with casper-fs-worker.  If not, see <http://www.gnu.org/licenses/>.
+* along with casper-job. If not, see <http://www.gnu.org/licenses/>.
 */
 #pragma once
 #ifndef CASPER_JOB_BASE_H_
